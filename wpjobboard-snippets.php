f129cda2a8cb3f5f5294d7c345ed8d18980440c5
Greg Winiarski <grzewin@gmail.com>
2016-05-28 16:28:06 +0200

    4.4.1 updates

--- wpjobboard-snippets.php
+++ wpjobboard-snippets.php
@@ -6,6 +6,8 @@ Author: Greg Winiarski
 Description: This is collection of useful and practical WPJobBoard snippets.
 */
 
+define( "WPJB_SNIPPETS", true );
+
 $file = dirname( __FILE__ ) . "/wpjobboard-tester.php";
 
 if( is_file( $file ) ) {
